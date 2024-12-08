6d99bc8e89e030bac9b85aae70528b0c62d34e48
Tabsheera-Shakeel <tabsheerasha123@gmail.com>
2024-12-09 00:36:25 +0500

    updating

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,18 +31,6 @@ export default function Home() {
       <Navbar/>
       <HeroSection/>
     <div className="bg-[#f6f7f9] min-h-screen p-4 sm:p-6 lg:p-20 flex flex-col gap-10 font-[family-name:var(--font-geist-sans)]">
-     
-      {/* <section className="first w-full flex flex-wrap sm:flex-nowrap gap-4 sm:gap-8 justify-center">
-        <Image src={"/Ads 1.png"} alt="" width={640} height={360} className="max-w-full" />
-        <Image src={"/Ads 2.png"} alt="" width={640} height={360} className="max-w-full" />
-      </section>
-
-    
-      <section className="w-full flex flex-wrap sm:flex-nowrap items-center justify-center sm:justify-between gap-4 sm:gap-8">
-        <Image src={"/Pick - Up.png"} alt="" width={582} height={132} className="max-w-full" />
-        <Image src={"/Switch.png"} alt="" width={60} height={60} className="max-w-full" />
-        <Image src={"/Drop - Off.png"} alt="" width={582} height={132} className="max-w-full" />
-      </section> */}
 
       
       <section className="popular w-full flex flex-col gap-4">
